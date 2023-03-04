c926d5d65e1d5b4f0702c94db455b5d0de681cf8
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-03-04 00:21:42 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Howard Thomas on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
